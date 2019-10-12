33d8215b0fcb45e56387e725bfabfe3676e598e5
Alexey Shevchenko <kamikozz@mail.ru>
2019-10-12 04:58:19 +0300

    Add process.env.PORT for heroku; routing to sites on request type 'message_new'

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+const https = require('https');
 const http = require('http');
+const fs = require('fs');
 
-const port = 3000;
-const host = "localhost";
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(function (req, res) {
   const ip = req.socket.remoteAddress;
   const port = req.socket.remotePort;
   console.error(`Connection established from: ${ip}:${port}. To:${req.url}`);
-
+  // res.writeHead(200, { 'Content-Type': 'text/plain' });
   switch (req.url) {
     case "/":
       res.end(`<div><div>Your IP address is <h4 style="display: inline; color:#FF0000">${ip}</h4></div><div>Your source port is <h4 style="display: inline; color:#FF0000">${port}</h4></div></div>`);
@@ -16,7 +15,7 @@ const server = http.createServer((req, res) => {
       if (req.method === 'POST') {
         const type = req.headers['content-type'];
         let data = '';
-        req.on('data', (chunk) => { data = chunk; });
+        req.on('data', (chunk) => { data += chunk; });
         req.on('end', () => {
           if (type && type.indexOf('application/json') !== -1) {
             console.error('Got: "application/json"');
@@ -34,10 +33,40 @@ const server = http.createServer((req, res) => {
             console.log(parsed);
             if (parsed.type && parsed.type === 'confirmation' &&
               parsed.group_id && parsed.group_id === '--VK_GROUP_ID--') {
-              console.log(req);
+              //console.log(req);
               res.writeHead(200);
               res.end('--VK_RESPONSE_KEY--');
               return;
+            }
+
+            if (parsed.type && parsed.type === 'message_new') {
+              res.writeHead(200);
+              res.end('ok');
+
+              //// Send request to test YANDEX.RU
+              const options = {
+                method: 'GET',
+                hostname: 'm.vk.com',
+                path: '/',
+                // headers: {
+                //   'Content-Type': 'application/json',
+                //   'Content-Length': data.length
+                // }
+              }
+              https.get(options, (res) => {
+                console.log('statusCode:', res.statusCode);
+                // console.log('headers:', res.headers);
+                let data = '';
+                res.on('data', (chunk) => {
+                  data += chunk;
+                  //process.stdout.write(chunk);
+                });
+                res.on('end', () => {
+                  console.log(data.toString());
+                });
+              }).on('error', (e) => {
+                console.error(e.message);
+              });
             }
           } else {
             console.log(data.toString());
@@ -60,6 +89,4 @@ const server = http.createServer((req, res) => {
       res.end(txt);
       break;
   }
-}).listen(port);
-
-console.error(`Server listens http://${host}:${port}`);
+}).listen(process.env.PORT || 5000);
