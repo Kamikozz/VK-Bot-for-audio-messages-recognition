b8aed127d30675d8d066c1ca4fe50287d979c624
Alexey Shevchenko <kamikozz@mail.ru>
2019-10-14 09:38:17 +0300

    Add responseWrapper; refactor; import config file; fix response message on new_message

--- server.js
+++ server.js
@@ -2,21 +2,32 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 
+const config = require('./credentials.json');
+
 const server = http.createServer(function (req, res) {
   const ip = req.socket.remoteAddress;
   const port = req.socket.remotePort;
-  console.error(`Connection established from: ${ip}:${port}. To:${req.url}`);
+  console.error(
+    `Connection established from: ${ip}:${port}. To:${req.url}`);
   // res.writeHead(200, { 'Content-Type': 'text/plain' });
+  let txt = '';
   switch (req.url) {
-    case "/":
-      res.end(`<div><div>Your IP address is <h4 style="display: inline; color:#FF0000">${ip}</h4></div><div>Your source port is <h4 style="display: inline; color:#FF0000">${port}</h4></div></div>`);
+    case "/test":
+      txt = `<div><div>Your IP address is <h4 style="display: inline; color:#FF0000">${ip}</h4></div><div>Your source port is <h4 style="display: inline; color:#FF0000">${port}</h4></div></div>`;
+      responseWrapper.call(res, txt, 200, {
+        'Content-Type': 'text/html; charset=UTF-8',
+        'Content-Length': Buffer.byteLength(txt)
+      });
       break;
     case "/callback":
       if (req.method === 'POST') {
-        const type = req.headers['content-type'];
         let data = '';
-        req.on('data', (chunk) => { data += chunk; });
+        // Copy Buffer stream to 'data'
+        req.on('data', (chunk) => {
+          data += chunk;
+        });
         req.on('end', () => {
+          const type = req.headers['content-type'];
           if (type && type.indexOf('application/json') !== -1) {
             console.error('Got: "application/json"');
             let parsed;
@@ -25,68 +36,79 @@ const server = http.createServer(function (req, res) {
             } catch (error) {
               if (error.name === 'SyntaxError') {
                 console.error(error.message);
-                res.writeHead(400);
-                res.end('error');
+                responseWrapper.call(res, 'Invalid JSON format', 400);
                 return;
               }
             }
             console.log(parsed);
-            if (parsed.type && parsed.type === 'confirmation' &&
-              parsed.group_id && parsed.group_id === '--VK_GROUP_ID--') {
-              //console.log(req);
-              res.writeHead(200);
-              res.end('--VK_RESPONSE_KEY--');
-              return;
-            }
 
-            if (parsed.type && parsed.type === 'message_new') {
-              res.writeHead(200);
-              res.end('ok');
+            if (parsed.type && parsed.group_id) {
+              if (parsed.group_id === config.vk_group_id) {
+                switch (parsed.type) {
+                  case 'confirmation':
+                    responseWrapper.call(res, config.vk_confirmation_code, 200);
+                    return;
+                  case 'message_new':
+                    responseWrapper.call(res, 'ok', 200);
 
-              //// Send request to test YANDEX.RU
-              const options = {
-                method: 'GET',
-                hostname: 'm.vk.com',
-                path: '/',
-                // headers: {
-                //   'Content-Type': 'application/json',
-                //   'Content-Length': data.length
-                // }
+                    //// Send request to test domen
+                    const options = {
+                      method: 'GET',
+                      hostname: 'm.vk.com',
+                      path: '/',
+                      //port: process.env.PORT || 5000
+                      // headers: {
+                      //   'Content-Type': 'application/json',
+                      //   'Content-Length': data.length
+                      // }
+                    }
+                    https.get(options, (res) => {
+                      console.error(res.statusCode);
+                      // console.error('headers:', res.headers);
+                      let data = '';
+                      res.on('data', (chunk) => {
+                        data += chunk;
+                        //process.stdout.write(chunk);
+                      });
+                      res.on('end', () => {
+                        console.log(data.toString());
+                      });
+                    }).on('error', (e) => {
+                      console.error(e.message);
+                    });
+                    return;
+                }
+              } else {
+                responseWrapper.call(res, 'ok', 200);
+                console.error('Incorrect group_id');
+                return;
               }
-              https.get(options, (res) => {
-                console.log('statusCode:', res.statusCode);
-                // console.log('headers:', res.headers);
-                let data = '';
-                res.on('data', (chunk) => {
-                  data += chunk;
-                  //process.stdout.write(chunk);
-                });
-                res.on('end', () => {
-                  console.log(data.toString());
-                });
-              }).on('error', (e) => {
-                console.error(e.message);
-              });
             }
           } else {
             console.log(data.toString());
           }
-          res.writeHead(200);
-          res.end('ok');
+          responseWrapper.call(res, 'ok', 200);
         });
       } else {
         // if not POST
-        res.writeHead(405);
-        res.end('error');
+        responseWrapper.call(res, 'Only POST method is allowed', 405);
       }
       break;
     default:
-      const txt = '404 not found';
-      res.writeHead(404, {
+      txt = '<h4>404 Not Found</h4>';
+      responseWrapper.call(res, txt, 404, {
         'Content-Type': 'text/html; charset=UTF-8',
         'Content-Length': Buffer.byteLength(txt)
       });
-      res.end(txt);
       break;
   }
 }).listen(process.env.PORT || 5000);
+
+function responseWrapper(message, statusCode, headers) {
+  if (statusCode) {
+    headers ?
+      this.writeHead(statusCode, headers) : this.writeHead(statusCode);
+  }
+  if (message) this.write(message);
+  this.end();
+}
